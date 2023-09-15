227890f816d6da901e0dfb6e2b7d9da2eb9f50e8
Prof. Dr. Peter A. Henning <peter@henning-weingarten.de>
2023-09-15 12:24:47 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "snapmaker_control"
+plugin_identifier = "snapmaker20_control"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_snapmaker_control"
+plugin_package = "octoprint_snapmaker20_control"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint - Snapmaker Control"
+plugin_name = "OctoPrint - Snapmaker 2.0 Control"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.0"
